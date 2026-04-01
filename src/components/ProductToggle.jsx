17e48a4cdee31841c7ebac04a89d7af16f6230e6
Kazi-Tafhimul <tafhimulkazi@gmail.com>
2026-04-01 12:29:06 +0600

    product toogle button done

--- src/components/ProductToggle.jsx
+++ src/components/ProductToggle.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const ProductToggle = () => {
     return (
-        <div>
+        <div className='flex justify-center items-center py-10'>
             {/* name of each tab group should be unique */}
-<div className="tabs tabs-box rounded-full flex justify-center items-center ">
-  <input type="radio" name="my_tabs_1" className="tab" aria-label="Tab 1" />
-  <input type="radio" name="my_tabs_1" className="tab" aria-label="Tab 2" defaultChecked />
-  <input type="radio" name="my_tabs_1" className="tab" aria-label="Tab 3" />
+<div className="tabs tabs-box bg-white border border-gray-200 rounded-full p-1">
+  <input type="radio" name="product_toggle" className="tab !rounded-full px-8 checked:bg-gradient-to-r from-blue-600 to-purple-600 checked:!text-white" aria-label="Products"/>
+  <input type="radio" name="product_toggle" className="tab !rounded-full px-8 checked:bg-gradient-to-r from-blue-600 to-purple-600 checked:!text-white" aria-label="Cart(2)" defaultChecked />
+  
 </div>
         </div>
     );
